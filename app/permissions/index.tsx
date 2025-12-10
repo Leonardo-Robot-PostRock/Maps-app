18464fd0248e52c61393e03c5106f713b044e028
Leonardo <leonardo38361@gmail.com>
2025-12-10 03:37:27 -0300

    pantalla para dar permisos de localización

--- app/permissions/index.tsx
+++ app/permissions/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { ThemedText } from '@/presentation/components/shared/themed-text'
+import ThemedPressable from '@/presentation/shared/ThemedPressable'
+import { usePermissionsStore } from '@/presentation/store/usePermissions'
+import { View } from 'react-native'
 
 const PermissionsScreen = () => {
-  return (
-    <View>
-      <Text>PermissionsScreen</Text>
-    </View>
-  )
+    const { locationStatus, requestLocationPermission } = usePermissionsStore()
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <ThemedPressable onPress={requestLocationPermission}>
+                Habilitar ubicación
+            </ThemedPressable>
+
+            <ThemedText>Estado actual: {locationStatus}</ThemedText>
+        </View>
+    )
 }
 
 export default PermissionsScreen
