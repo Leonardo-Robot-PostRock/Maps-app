89b23fb2109dbcb93a5d93ed79878919356c28f9
Leonardo <leonardo38361@gmail.com>
2025-12-11 18:37:27 -0300

    instalación de react native maps

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >Map Screen</MapView>
     </View>
   )
 }
 
-export default MapScreen
+export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+})
