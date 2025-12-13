81df48d621c99feb43ca0e9ad40b450db2b9d22f
Leonardo <leonardo38361@gmail.com>
2025-12-12 21:25:17 -0300

    se agrega google provider

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,18 +1,19 @@
 import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const MapScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <MapView
         style={styles.map}
+        provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      >Map Screen</MapView>
+      />
     </View>
   )
 }
