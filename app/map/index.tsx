98efe6bdf5bc5b493e0664593f87d852313829b0
Leonardo <leonardo38361@gmail.com>
2025-12-12 23:51:54 -0300

    cambio de coordenadas

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -7,9 +7,10 @@ const MapScreen = () => {
       <MapView
         style={styles.map}
         provider={PROVIDER_GOOGLE}
+        // -33.039058, -68.879791
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: -33.039058,
+          longitude: -68.879791,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
