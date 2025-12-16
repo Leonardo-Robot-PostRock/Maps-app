fdb3a0b6650e7052ef174825a9756fadb258dafe
Leonardo <leonardo38361@gmail.com>
2025-12-16 19:05:09 -0300

    botón flotante

---
+++ presentation/components/shared/FAB.tsx
@@ -0,0 +1,42 @@
+import { Ionicons } from '@expo/vector-icons';
+import { StyleProp, StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native';
+
+interface Props {
+    iconName: keyof typeof Ionicons.glyphMap;
+    onPress: () => void;
+    style?: StyleProp<ViewStyle>;
+}
+
+const FAB = ({ iconName, onPress, style }: Props) => {
+    return (
+        <View>
+            <TouchableOpacity
+                style={[styles.btn, style]}
+                onPress={onPress}
+            >
+                <Ionicons name={iconName} size={40} color='white' />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default FAB;
+
+const styles = StyleSheet.create({
+    btn: {
+        zIndex: 1,
+        position: 'absolute',
+        height: 50,
+        width: 50,
+        borderRadius: 30,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowOpacity: 0.3,
+        shadowOffset: {
+            height: 0.27,
+            width: 4.5
+        },
+        elevation: 5
+    }
+})
