636dfddf856fdebaa5134ce6c8bbcb95a366d7f0
Leonardo <leonardo38361@gmail.com>
2025-12-16 00:13:40 -0300

    creación de store para location

---
+++ presentation/store/useLocationStore.ts
@@ -0,0 +1,47 @@
+import { getCurrentLocation, watchCurrentPosition } from "@/core/actions/location/location";
+import { LatLng } from "@/infrastructure/interfaces/lat-lng";
+import { LocationSubscription } from "expo-location";
+import { create } from "zustand";
+
+interface LocationState {
+    lastKnownLocation: LatLng | null;
+    userLocationList: LatLng[];
+    watchSubscriptionID: LocationSubscription | null;
+    getLocation: () => Promise<LatLng>;
+    watchLocation: () => void;
+    clearWatchLocation: () => void;
+}
+
+export const useLocationStore = create<LocationState>()((set, get) => ({
+    lastKnownLocation: null,
+    userLocationList: [],
+    watchSubscriptionID: null,
+    getLocation: async () => {
+        const location = await getCurrentLocation();
+        set({ lastKnownLocation: location })
+        return location;
+
+    },
+    watchLocation: async () => {
+        const oldSubscription = get().watchSubscriptionID;
+
+        if (oldSubscription) {
+            get().clearWatchLocation()
+        }
+
+        const watchSubscription = await watchCurrentPosition((latLng) => {
+            set({
+                lastKnownLocation: latLng,
+                userLocationList: [...get().userLocationList, latLng]
+            })
+        });
+        set({ watchSubscriptionID: watchSubscription })
+    },
+    clearWatchLocation: () => {
+        const subscription = get().watchSubscriptionID;
+
+        if (subscription !== null) {
+            subscription.remove();
+        }
+    }
+}))
